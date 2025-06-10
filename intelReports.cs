3562bf07e9f731074a205006e6bd1af8e5b36b52
Yakir Uzan <yakiruzangreen@gmail.com>
2025-06-10 11:30:33 +0300

    added 2 fils & func

---
+++ intelReports.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon
+{
+    internal class intelReports
+    {
+        public int id { get; set; }
+        public int reporter_id { get; set; }
+        public int target_id { get; set; }
+        public string text { get; set; }
+        public DateTime timestamp { get; set; }
+    
+
+    public intelReports(int id, int reporter_id, int target_id, string text, DateTime timestamp)
+        {
+            this.id = id;
+            this.reporter_id = reporter_id;
+            this.target_id = target_id;
+            this.text = text;
+            this.timestamp = timestamp;
+        }
+    }
+}
