06e2701bb52904fca75d0e946ffaffdad40d545a
Yakir Uzan <yakiruzangreen@gmail.com>
2025-06-10 12:08:16 +0300

    added maneger file

---
+++ MalshinonMng.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Malshinon
+{
+    internal class MalshinonMng
+    {
+        MalshinonDAL dal = new MalshinonDAL();
+
+        public void MngPersonFlow()
+        {
+            Console.WriteLine("Enter your first Name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last Name: ");
+            string lastName = Console.ReadLine();
+        }
+
+
+    }
+}
